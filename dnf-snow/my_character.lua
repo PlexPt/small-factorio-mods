62b78592430dfbdd1eae7ec6b4e5ae98de2eeb7f
plexpt <plexpt@gmail.com>
2024-11-27 05:06:06 +0800

    520

--- dnf-snow/my_character.lua
+++ dnf-snow/my_character.lua
@@ -1,12 +1,14 @@
 require("lib.animation_util")
 local util = require("util")
-local common = require('lib.common')
+--~ local common = require('lib.common')
 
-local IMG_PATH = common.IMG_PATH
+--~ local IMG_PATH = common.IMG_PATH
+local IMG_PATH = DNF_SNOW.IMG_PATH
 
 local char_icon = IMG_PATH .. "idle/45.png"
 
-local char_name = common.char_name
+--~ local char_name = require("name")
+
 
 
 local function getIdlePics(max)
@@ -98,7 +100,6 @@ local function getRunningGun()
         scale = 0.3,
     }
     return s
-
 end
 
 local function getIdle()
@@ -176,160 +177,169 @@ animations_dead = {
 }
 
 
---Character definition
-data:extend(
-        {
-            {
-                type = "character-corpse",
-                name = char_name .. "-corpse",
-                icon = char_icon,
-                icon_size = 400,
-                minable = { mining_time = 2 },
-                time_to_live = 15 * 60 * 60, -- 15 minutes
-                selection_box = { { -0.7, -0.7 }, { 0.7, 0.7 } },
-                selection_priority = 100, -- 0-255 value with 255 being on-top of everything else
-                flags = { "placeable-off-grid", "not-rotatable", "not-on-map" },
-                pictures = animations_dead,
-                -- The highest index found in the corpse is the graphics variation used
-                armor_picture_mapping = {
-                    --nil = 1,
-                    ["light-armor"] = 2,
-                    ["heavy-armor"] = not data.is_demo and 2 or nil,
-                    ["modular-armor"] = not data.is_demo and 3 or nil,
-                    ["power-armor"] = not data.is_demo and 3 or nil,
-                    ["power-armor-mk2"] = not data.is_demo and 3 or nil
-                }
-            },
-            {
-                type = "character",
-                name = char_name,
-                -- character_corpse will be replaced in data-final-fixes.lua, if necessary!
-                character_corpse = "character-corpse",
-                icon = char_icon,
-                icon_size = 400,
-                flags = { "placeable-off-grid", "breaths-air", "not-repairable", "not-on-map", "not-flammable" },
-                max_health = 250,
-                alert_when_damaged = false,
-                healing_per_tick = 0.15,
-                collision_box = { { -0.2, -0.2 }, { 0.2, 0.2 } },
-                selection_box = { { -0.4, -1.4 }, { 0.4, 0.2 } },
-                hit_visualization_box = { { -0.2, -1.1 }, { 0.2, 0.2 } },
-                sticker_box = { { -0.2, -1 }, { 0.2, 0 } },
-                crafting_categories = { "crafting" },
-                mining_categories = { "basic-solid" },
-                inventory_size = 100,
-                build_distance = 100,
-                drop_item_distance = 100,
-                reach_distance = 100,
-                item_pickup_distance = 10,
-                loot_pickup_distance = 20,
-                enter_vehicle_distance = 3,
-                reach_resource_distance = 2.7,
-                ticks_to_keep_gun = 600,
-                ticks_to_keep_aiming_direction = 100,
-                moving_sound_animation_positions = {10, 21},
-                --ticks you need to wait after firing a weapon or taking damage to get out of combat and get healed
-                ticks_to_stay_in_combat = 600,
-                damage_hit_tint = { r = 1, g = 0, b = 0, a = 0 },
-                running_speed = 0.15,
-                distance_per_frame = 0.13,
-                maximum_corner_sliding_distance = 0.7,
-                subgroup = "creatures",
-                order = "a",
-                eat = {
-                    {
-                        filename = "__base__/sound/eat.ogg",
-                        volume = 1
-                    }
-                },
-                heartbeat = {
-                    {
-                        filename = "__base__/sound/heartbeat.ogg"
-                    }
-                },
-                tool_attack_result = {
-                    type = "direct",
-                    action_delivery = {
-                        type = "instant",
-                        target_effects = {
-                            type = "damage",
-                            damage = { amount = 8, type = "physical" }
-                        }
-                    }
-                },
+------------------------------------------------------------------------------------
+--                                Character corpse                                --
+------------------------------------------------------------------------------------
+local corpse = DNF_SNOW.protos.corpse
+corpse.localised_name            = {"entity-name."..corpse.name}
 
-                animations = animations_live,
-                light = {
-                    {
-                        minimum_darkness = 0.3,
-                        intensity = 0.4,
-                        size = 25,
-                        color = { r = 1.0, g = 1.0, b = 1.0 }
-                    },
-                    {
-                        type = "oriented",
-                        minimum_darkness = 0.3,
-                        picture = {
-                            filename = "__core__/graphics/light-cone.png",
-                            priority = "extra-high",
-                            flags = { "light" },
-                            scale = 2,
-                            width = 200,
-                            height = 200
-                        },
-                        shift = { 0, -13 },
-                        size = 2,
-                        intensity = 0.6,
-                        color = { r = 1.0, g = 1.0, b = 1.0 }
-                    }
-                },
-                mining_speed = 0.5,
-                mining_with_tool_particles_animation_positions = { 19 },
-                running_sound_animation_positions = { 5, 16 },
-                footstep_particle_triggers = {
-                    {
-                        tiles = { "water-shallow" },
+--~ data:extend(
+        --~ {
+            --~ {
+                --~ type = "character-corpse",
+                --~ name = char_name .. "-corpse",
+corpse.icon = char_icon
+corpse.icon_size = 400
+--~ corpse.minable = { mining_time = 2 }
+corpse.time_to_live = 15 * 60 * 60                   -- 15 minutes
+--~ corpse.selection_box = { { -0.7, -0.7 }, { 0.7, 0.7 } }
+--~ corpse.selection_priority = 100 -- 0-255 value with 255 being on-top of everything else
+--~ corpse.flags = { "placeable-off-grid", "not-rotatable", "not-on-map" }
+corpse.pictures = animations_dead
+-- The highest index found in the corpse is the graphics variation used
+corpse.armor_picture_mapping = {
+  --nil = 1,
+  ["light-armor"] = 2,
+  ["heavy-armor"] = not data.is_demo and 2 or nil,
+  ["modular-armor"] = not data.is_demo and 3 or nil,
+  ["power-armor"] = not data.is_demo and 3 or nil,
+  ["power-armor-mk2"] = not data.is_demo and 3 or nil
+}
 
-                        type = "create-particle",
-                        repeat_count = 5,
-                        particle_name = "shallow-water-droplet-particle",
-                        initial_height = 0.2,
-                        speed_from_center = 0.01,
-                        speed_from_center_deviation = 0.05,
-                        initial_vertical_speed = 0.02,
-                        initial_vertical_speed_deviation = 0.05,
-                        offset_deviation = { { -0.2, -0.2 }, { 0.2, 0.2 } }
-                    },
-                    {
-                        tiles = { "water-mud" },
 
-                        type = "create-particle",
-                        repeat_count = 5,
-                        particle_name = "shallow-water-droplet-particle",
-                        initial_height = 0.2,
-                        speed_from_center = 0.01,
-                        speed_from_center_deviation = 0.05,
-                        initial_vertical_speed = 0.02,
-                        initial_vertical_speed_deviation = 0.05,
-                        offset_deviation = { { -0.2, -0.2 }, { 0.2, 0.2 } }
-                    }
-                },
-                water_reflection = {
-                    pictures = {
-                        filename = IMG_PATH .. "character-reflection.png",
-                        priority = "extra-high",
-                        -- flags = { "linear-magnification", "not-compressed" },
-                        -- default value: flags = { "terrain-effect-map" },
-                        width = 13,
-                        height = 19,
-                        shift = util.by_pixel(0, 67 * 0.5),
-                        scale = 5,
-                        variation_count = 1
-                    },
-                    rotate = false,
-                    orientation_to_variation = false
-                }
-            } })
+------------------------------------------------------------------------------------
+--                                    Character                                   --
+------------------------------------------------------------------------------------
+local char = DNF_SNOW.protos.character
+
+char.localised_name         = {"entity-name."..char.name}
+char.localised_description  = {"entity-description."..char.name}
+                --~ -- character_corpse will be replaced in data-final-fixes.lua, if necessary!
+                --~ character_corpse = "character-corpse",
+char.icon = char_icon
+char.icon_size = 400
+                --~ flags = { "placeable-off-grid", "breaths-air", "not-repairable", "not-on-map", "not-flammable" },
+--~ char.max_health = 250
+--~ char.alert_when_damaged = false
+--~ char.healing_per_tick = 0.15
+--~ char.collision_box = { { -0.2, -0.2 }, { 0.2, 0.2 } }
+--~ char.selection_box = { { -0.4, -1.4 }, { 0.4, 0.2 } }
+--~ char.hit_visualization_box = { { -0.2, -1.1 }, { 0.2, 0.2 } }
+--~ char.sticker_box = { { -0.2, -1 }, { 0.2, 0 } }
+--~ char.crafting_categories = { "crafting" }
+--~ char.mining_categories = { "basic-solid" }
+char.inventory_size = 100
+char.build_distance = 100
+char.drop_item_distance = 100
+char.reach_distance = 100
+char.item_pickup_distance = 10
+char.loot_pickup_distance = 20
+--~ char.enter_vehicle_distance = 3
+--~ char.reach_resource_distance = 2.7
+--~ char.ticks_to_keep_gun = 600
+--~ char.ticks_to_keep_aiming_direction = 100
+--~ char.moving_sound_animation_positions = {10, 21}
+--ticks you need to wait after firing a weapon or taking damage to get out of combat and get healed
+--~ char.ticks_to_stay_in_combat = 600
+char.damage_hit_tint = { r = 1, g = 0, b = 0, a = 0 }   -- Default: r = 0.12
+--~ char.running_speed = 0.15
+--~ char.distance_per_frame = 0.13
+--~ char.maximum_corner_sliding_distance = 0.7
+--~ char.subgroup = "creatures"
+                --~ order = "a",
+--~ char.eat = {
+ --~ {
+    --~ filename = "__base__/sound/eat-1.ogg",
+    --~ volume = 1
+  --~ }
+--~ },
+--~ char.heartbeat = {
+  --~ {
+      --~ filename = "__base__/sound/heartbeat.ogg"
+  --~ }
+--~ }
+--~ char.tool_attack_result = {
+  --~ type = "direct",
+  --~ action_delivery = {
+    --~ type = "instant",
+    --~ target_effects = {
+        --~ type = "damage",
+        --~ damage = { amount = 8, type = "physical" }
+    --~ }
+  --~ }
+--~ }
+
+char.animations = animations_live
+                --~ light = {
+                    --~ {
+                        --~ minimum_darkness = 0.3,
+                        --~ intensity = 0.4,
+                        --~ size = 25,
+                        --~ color = { r = 1.0, g = 1.0, b = 1.0 }
+                    --~ },
+                    --~ {
+                        --~ type = "oriented",
+                        --~ minimum_darkness = 0.3,
+                        --~ picture = {
+                            --~ filename = "__core__/graphics/light-cone.png",
+                            --~ priority = "extra-high",
+                            --~ flags = { "light" },
+                            --~ scale = 2,
+                            --~ width = 200,
+                            --~ height = 200
+                        --~ },
+                        --~ shift = { 0, -13 },
+                        --~ size = 2,
+                        --~ intensity = 0.6,
+                        --~ color = { r = 1.0, g = 1.0, b = 1.0 }
+                    --~ }
+                --~ }
+                --~ mining_speed = 0.5,
+                --~ mining_with_tool_particles_animation_positions = { 19 },
+                --~ running_sound_animation_positions = { 5, 16 },
+char.footstep_particle_triggers = {
+  {
+    tiles = { "water-shallow" },
+
+    type = "create-particle",
+    repeat_count = 5,
+    particle_name = "shallow-water-droplet-particle",
+    initial_height = 0.2,
+    speed_from_center = 0.01,
+    speed_from_center_deviation = 0.05,
+    initial_vertical_speed = 0.02,
+    initial_vertical_speed_deviation = 0.05,
+    offset_deviation = { { -0.2, -0.2 }, { 0.2, 0.2 } }
+  },
+  {
+    tiles = { "water-mud" },
+    type = "create-particle",
+    repeat_count = 5,
+    particle_name = "shallow-water-droplet-particle",
+    initial_height = 0.2,
+    speed_from_center = 0.01,
+    speed_from_center_deviation = 0.05,
+    initial_vertical_speed = 0.02,
+    initial_vertical_speed_deviation = 0.05,
+    offset_deviation = { { -0.2, -0.2 }, { 0.2, 0.2 } }
+  }
+}
+char.water_reflection = {
+  pictures = {
+    filename = IMG_PATH .. "character-reflection.png",
+    priority = "extra-high",
+    -- flags = { "linear-magnification", "not-compressed" },
+    -- default value: flags = { "terrain-effect-map" },
+    width = 13,
+    height = 19,
+    shift = util.by_pixel(0, 67 * 0.5),
+    scale = 5,
+    variation_count = 1
+  },
+  rotate = false,
+  orientation_to_variation = false
+}
+  --~ } })
 
 --if ((data.raw["character"]) and (data.raw["character"]["character"])) then
 --    data.raw["character"]["character"]["animations"] = animations_live
@@ -338,4 +348,3 @@ data:extend(
 --if ((data.raw["character-corpse"]) and (data.raw["character-corpse"]["character-corpse"])) then
 --    data.raw["character-corpse"]["character-corpse"]["pictures"] = animations_dead
 --end
-
